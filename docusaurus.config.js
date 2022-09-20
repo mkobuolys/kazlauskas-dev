376aad5d9aab2db837761f11f05e42e760db088d
Mangirdas Kazlauskas <mangirdas@kazlauskas.dev>
2022-09-21 00:15:53 +0300

    Update website theme colors

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,8 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const lightCodeTheme = require("prism-react-renderer/themes/vsLight");
+const darkCodeTheme = require("prism-react-renderer/themes/vsDark");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -50,6 +50,7 @@ const config = {
       colorMode: {
         respectPrefersColorScheme: true,
       },
+      image: "img/mkobuolys.png",
       navbar: {
         title: "kazlauskas.dev",
         logo: {
